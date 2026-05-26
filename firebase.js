20be8522523df09867516d4c5113c92097572151
Srilaxmi Vengala <vengalasrilaxmi@gmail.com>
2026-05-26 15:31:46 +0530

    fixed firebase config

--- firebase.js
+++ firebase.js
@@ -8,7 +8,7 @@ storageBucket: "memorymind-ai-7748c.firebasestorage.app",
 messagingSenderId: "153493663214",
 appId: "1:153493663214:web: 54073d4e1788570ca232ff",
 measurementId: "G-JQNFSCWPZ3"
-3;
+};
 
 const app = initializeApp(firebaseConfig);
 
