e3a8a96edab63a3d218de3cafe4a71cec67723e0
Srilaxmi Vengala <vengalasrilaxmi@gmail.com>
2026-05-26 15:26:40 +0530

    firebase setup

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+çonst firebaseConfig = {
+apikey: "AIzaSyBaw6QKzIXqd299Fks9RPLI9heQ3Alq0Aw"
+authDomain: "memorymind-ai-7748c.firebaseapp.com",
+projectId: "memorymind-ai-7748c",
+storageBucket: "memorymind-ai-7748c.firebasestorage.app",
+messagingSenderId: "153493663214",
+appId: "1:153493663214:web: 54073d4e1788570ca232ff",
+measurementId: "G-JQNFSCWPZ3"
+3;
+
+const app = initializeApp(firebaseConfig);
+
+console.log("Firebase Connected");
