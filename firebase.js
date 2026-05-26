5d05836133bd0be7cb8411e393bf03905b5cfdf0
Srilaxmi Vengala <vengalasrilaxmi@gmail.com>
2026-05-26 15:50:33 +0530

    fixed firebase config

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 const app = initializeApp(firebaseConfig);
 
-çonst firebaseConfig = {
-apikey: "AIzaSyBaw6QKzIXqd299Fks9RPLI9heQ3Alq0Aw"
-authDomain: "memorymind-ai-7748c.firebaseapp.com",
-projectId: "memorymind-ai-7748c",
-storageBucket: "memorymind-ai-7748c.firebasestorage.app",
-messagingSenderId: "153493663214",
+const firebaseConfig = {
+  apiKey: "AIzaSyBaw6QKzIXqd299Fks9RPLI9heQ3Alq0Aw",
+  authDomain: "memorymind-ai-7748c.firebaseapp.com",
+  projectId: "memorymind-ai-7748c",
+  storageBucket: "memorymind-ai-7748c.firebasestorage.app",
+  messagingSenderId: "153493663214",
 appId: "1:153493663214:web: 54073d4e1788570ca232ff"
 };
 
