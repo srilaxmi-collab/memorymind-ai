e9b0926ba8441dbd9cfd676622969310f94a5fc0
Srilaxmi Vengala <vengalasrilaxmi@gmail.com>
2026-05-26 15:37:50 +0530

    fixed firebase config

--- firebase.js
+++ firebase.js
@@ -6,8 +6,7 @@ authDomain: "memorymind-ai-7748c.firebaseapp.com",
 projectId: "memorymind-ai-7748c",
 storageBucket: "memorymind-ai-7748c.firebasestorage.app",
 messagingSenderId: "153493663214",
-appId: "1:153493663214:web: 54073d4e1788570ca232ff",
-measurementId: "G-JQNFSCWPZ3"
+appId: "1:153493663214:web: 54073d4e1788570ca232ff"
 };
 
 const app = initializeApp(firebaseConfig);
